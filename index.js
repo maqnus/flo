cad8bd349a9242f7c11a170ef94d19ec1dd4e4bd
Magnus Kjelland <magnus.kjelland@inmeta.no>
2019-10-04 14:03:44 +0200

    meta viewport fix

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var app = require('express')();
+var express = require('express'); 
+var app = express(); 
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000)); 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('public')); 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
@@ -22,5 +25,5 @@ io.on('connection', function(socket) {
 })
 
 http.listen(app.get('port'), function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:5000');
 });
