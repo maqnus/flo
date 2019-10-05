ccc9ca58529550bbb284d5f8d01096a5542906ca
Magnus Kjelland <magnus.kjelland@inmeta.no>
2019-10-05 12:26:24 +0200

    add pages

--- index.js
+++ index.js
@@ -7,8 +7,24 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public')); 
 
+app.get('/register', function(req, res){
+  res.sendFile(__dirname + '/public/register.html');
+});
+
+app.get('/reset-password', function(req, res){
+  res.sendFile(__dirname + '/public/reset-password.html');
+});
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+app.get('/mypage', function(req, res){
+  res.sendFile(__dirname + '/public/mypage.html');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/public/chat.html');
 });
 
 io.on('connection', function(socket) {
