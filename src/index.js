069edc7dd09e649ce454083c820bc3155a98ebb1
Magnus Kjelland <magnus.kjelland@inmeta.no>
2019-10-06 10:37:04 +0200

    refactgoring

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@
 //   }
   
 //   document.body.appendChild(component());
+const countdownWrapper = document.querySelector('.countdown');
+const countdownToggleButton = document.querySelector('.countdown-toggler');
+const toggleOpenCountdown = () => {
+  if (navigationWrapper.classList.contains('open')) {
+    navigationWrapper.classList.remove("open");
+  }
+  countdownWrapper.classList.toggle("open");
+}
+countdownToggleButton.addEventListener('click', toggleOpenCountdown);
+
 const navigationWrapper = document.querySelector('.main-nav');
 const navigationToggleButton = document.querySelector('.nav-toggler');
 const toggleOpenNavigation = () => {
+  if (countdownWrapper.classList.contains('open')) {
+    countdownWrapper.classList.remove("open");
+  }
   navigationWrapper.classList.toggle("open");
 }
   navigationToggleButton.addEventListener('click', toggleOpenNavigation);
